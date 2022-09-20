ce4a70f6c5b445585474b8ed30d3210610949db3
Pahontu Stefan-Alin <alin_pahontu@yahoo.com>
2022-09-20 16:16:08 +0300

    tried adding url params

--- PerformanceTool/main.js
+++ PerformanceTool/main.js
@@ -183,6 +183,7 @@ async function mainJS() {
                             updateGraph(curTest);
                         }
                     }
+                    permalinkFlavors();
                 });
             selection.append("label")
                 .attr("class", "form-check-label")
@@ -271,27 +272,7 @@ async function mainJS() {
                 .append("details")
                 .attr("id", tasks[i] + "collapsible")
                 .on("click", function () {
-                    let url = new URL(decodeURI(window.location));
-                    let test = url.searchParams.getAll("tasks");
-
-                    if (test.includes(tasks[i])) {
-                        test.splice(test.indexOf(tasks[i]), 1);
-                    } else {
-                        test.push(tasks[i]);
-                    }
-                    let test2 = url.search;
-                    console.log(test2);
-
-                    url.search = new URLSearchParams({
-                        "tasks": test.length === 0 ? [] : test,
-                        "commitFrom": url.searchParams.getAll('commitFrom'),
-                        "commitTo": url.searchParams.getAll('commitTo'),
-                        "flavors": url.searchParams.getAll("flavors"),
-                    })
-
-
-                    let stateObj = { id: "1" };
-                    window.history.replaceState("", "", url.toString());
+                    permalinkTask(tasks[i]);
                 });
             collapsible.append("summary")
                 .html(tasks[i]);
@@ -317,6 +298,7 @@ async function mainJS() {
                     updateDataOnDates(testsData[i], firstCommit.time, lastCommit.time);
                     updateGraph(testsData[i]);
                 }
+                permalinkDates(firstCommit.time, lastCommit.time);
             }
             testData.dataGroup.select(".brush").call(testData.brush.move, null);
         }
@@ -439,6 +421,7 @@ async function mainJS() {
     function selectFlavorsPreset(filter) {
         let wantedFlavors = flavors.filter(flavor => flavor.includes(filter));
         updateGraphsByFlavor(wantedFlavors);
+        permalinkFlavors();
     }
 
     function regexUpdate(filter) {
@@ -474,7 +457,6 @@ async function mainJS() {
                 startDate.setDate(endDate.getDate() - 14);
                 break;
         }
-
         for (let i = 0; i < numTests; i++) {
             updateDataOnDates(testsData[i], startDate, endDate);
             updateGraph(testsData[i]);
@@ -482,7 +464,73 @@ async function mainJS() {
 
         document.getElementById("startDate").valueAsDate = startDate;
         document.getElementById("endDate").valueAsDate = endDate;
+
+        permalinkDates(startDate, endDate);
     }
+
+    function permalinkDates(startDate, endDate) {
+        let url = new URL(decodeURI(window.location));
+        let params = new URLSearchParams(url.search);
+        params.set("startDate", startDate.toLocaleDateString('en-US'));
+        params.set("endDate", endDate.toLocaleDateString('en-US'));
+        url.search = params;
+        window.history.replaceState("", "", url.toString());
+    }
+
+    function permalinkTask(openTask) {
+        let url = new URL(decodeURI(window.location));
+        let params = new URLSearchParams(url.search);
+        let task = params.getAll("task");
+        task.splice(task.indexOf(""), 1);
+        params.set("task", task.length === 0 ? openTask : null);
+        url.search = params;
+        window.history.replaceState("", "", url.toString());
+    }
+
+    function permalinkFlavors() {
+        let url = new URL(decodeURI(window.location));
+        let params = new URLSearchParams(url.search);
+        let urlFlavor = params.get("flavors");
+        console.log(urlFlavor);
+        let availableFlavors = [];
+        if (urlFlavor === null) {
+            availableFlavors = testsData[0].availableFlavors.map(function (d) {
+                return flavors.indexOf(d);
+            });
+        }
+        else {
+            availableFlavors = urlFlavor.split(",").map(function (d) {
+                return +d;
+            });
+        }
+        console.log(availableFlavors);
+        params.set("flavors", availableFlavors);
+        url.search = params;
+        window.history.replaceState("", "", url.toString());
+    }
+
+    /*function decodeURL() {
+        let url = new URL(decodeURI(window.location));
+        let params = new URLSearchParams(url.search);
+        let task = params.get("task");
+        let startDate = new Date(params.get("startDate"));
+        let endDate = new Date(params.get("endDate"));
+        let urlFlavors = params.get("flavors");
+        let availableFlavors = urlFlavors === null ? flavors : urlFlavors.split(',').map(function (d) {
+            return flavors[d];
+        });
+        console.log(availableFlavors);
+        updateCheckboxes(availableFlavors);
+        for (let i = 0; i < numTests; i++) {
+            let curTest = testsData[i];
+            curTest.availableFlavors = availableFlavors;
+            updateDataOnDates(curTest, startDate, endDate);
+            updateGraph(curTest);
+        }
+        if (task !== null) {
+            document.getElementById(task + "collapsible").open = true;
+        }
+    }*/
 
     function addDatePickers(firstDatePicker, secondDatePicker, submitButton) {
         let startDate = null;
@@ -511,6 +559,7 @@ async function mainJS() {
                         updateDataOnDates(testsData[i], startDate, endDate);
                         updateGraph(testsData[i]);
                     }
+                    permalinkDates(startDate, endDate);
                 }
             }
         });
@@ -635,17 +684,6 @@ async function mainJS() {
         }
     }
 
-    function decodeURL() {
-        var url = new URL(decodeURI(window.location));
-        let tasks = url.searchParams.getAll('task');
-    }
-
-    function updateURL() {
-        let url = new URL(decodeURI(window.location));
-        let tasks = new URL("/tasks", url);
-        window.location.href = tasks;
-    }
-
     const exports = await App.MONO.mono_wasm_get_assembly_exports("PerformanceTool.dll");
     const promise = exports.Program.LoadData(measurementsUrl);
     let value = await promise;
@@ -682,11 +720,10 @@ async function mainJS() {
         let [filename, text] = createMarkdown([...testToTask.keys()].sort());
         download(filename, text);
     });
-    decodeURL();
     document.querySelector("#loadingCircle").style.display = 'none';
     document.querySelector("#main").style.display = '';
-
-    await runMainAndExit(config.mainAssemblyName, []);
+    decodeURL();
+    await App.MONO.mono_run_main("PerformanceTool.dll", applicationArguments);
 }
 
 await mainJS();
